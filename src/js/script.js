75441be01b453c6d82569be48716cf48533b1023
Minh nhut <nhut@rabi.co.jp>
2023-03-20 18:10:37 +0700

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -47,88 +47,11 @@ $('a[href*="#"]')
     }
   });
 
-
-  
-
-$(function () {
-
-  var $array_1 = [];
-  var $array_2 = [];
-  var $position = 1.3;
-  var $classArray = [];
-
-
-  // ﾃｩ窶ｦﾂ催･ﾋ�汎�堋ｵﾄθ陳ｳﾄθ停汎θ陳ｫ
-  //var $classArray = [$(".p-ani_"+1), $(".p-ani_2")];
-
-  // ﾃｩ窶ｦﾂ催･ﾋ�� pushﾄ�堋ｵﾄθ陳ｳﾄθ停汎θ陳ｫ
-  // for (var i = 1; i <= 6; i++) {
-  //   $classArray.push($(".p-ani-fadeinup_" + i));
-  //   $(".p-ani-fadeinup_" + i).css("visibility", "hidden");
-  // }
-
-  // ﾃｩ窶ｦﾂ催･ﾋ�杷orﾃｦ窶凪｡ﾄ�堋ｵﾄθ陳ｳﾄθ停汎θ陳ｫ
-  /*for (var i in $hoge[index]) {}*/
-
-
-  //classArrayﾄδ�ｫﾄ�堋ｯﾄθ陳ｩﾄ�堋ｹﾃ･ﾂ青催ｨﾂｿﾂｽﾃ･ﾂ� 
-  $classArray.push($(".p-fadeinup"));
-  $(".p-fadeinup").css("visibility", "hidden");
-
-  $classArray.push($(".p-fadein"));
-  $(".p-fadein").css("visibility", "hidden");
-
-  $classArray.push($(".p-width"));
-  $(".p-width").css("visibility", "hidden");
-
-  $classArray.push($(".p-fadeinblur"));
-  $(".p-fadeinblur").css("visibility", "hidden");
-
-  $classArray.push($(".p-fadeinscale"));
-  $(".p-fadeinscale").css("visibility", "hidden");
-
-  $classArray.push($(".p-height"));
-  $(".p-height").css("visibility", "hidden");
-
-  $classArray.push($(".p-fadeinleft"));
-  $(".p-fadeinleft").css("visibility", "hidden");
-
-
-
-  //classArrayﾄδ�ｮﾃｩ窶ｦﾂ催･ﾋ�氾･ﾋ��ﾃ､ﾂｻﾂ｣ﾃ･窶ｦﾂ･
-  for (var i in $classArray) {
-    $.each($classArray[i], function (index, element) {
-      $array_1.push(element);
-    });
-    $array_2.push($array_1);
-    $array_1 = [];
-  }
-
-
-  //CSS Animation
-  $(window).on("load scroll", function () {
-    for (var i in $classArray) {
-
-      $.each($classArray[i], function (index, element) {
-        // console.log(element);
-        aniFunc($(this), index, i);
-      });
-    }
-  });
-
-  function aniFunc($target, junban, type) {
-    var windowHeight = $(window).height(),
-      topWindow = $(window).scrollTop(),
-      adjustHeight = (windowHeight / $position).toFixed(0),
-      targetPosition;
-
-    $target.each(function () {
-      targetPosition = $target.offset().top;
-
-      if (topWindow > targetPosition - adjustHeight) {
-        if (type == 0) { $($array_2[type][junban]).addClass("p-ani-fadeInUp"); } else if (type == 1) { $($array_2[type][junban]).addClass("p-ani-fadeIn"); } else if (type == 2) { $($array_2[type][junban]).addClass("p-ani-width"); } else if (type == 3) { $($array_2[type][junban]).addClass("p-ani-fadeInBlur"); } else if (type == 4) { $($array_2[type][junban]).addClass("p-ani-fadeInScale"); } else if (type == 5) { $($array_2[type][junban]).addClass("p-ani-height"); } else if (type == 6) { $($array_2[type][junban]).addClass("p-ani-fadeInLeft"); }
-
-      }
-    });
-  }
-});
+$(document).ready(function () {
+  var hamburger = $(".hamburger");
+  var menu = $(".c-menu");
+  hamburger.click(function () {
+    menu.fadeToggle();
+    hamburger.toggleClass('active')
+  })
+})
